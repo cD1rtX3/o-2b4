65e1e919aaf1b4d56ad6080cdc8bd947c068ea9d
cD1rtX3 <113859223+cD1rtX3@users.noreply.github.com>
2024-02-06 09:50:41 -0500

    Make `ctx` not readonly

--- draw.js
+++ draw.js
@@ -23,23 +23,26 @@
  */
 class Canvas {
 	/** 
- 	 * Canvas context object. Private, readonly field.
- 	 */
-	#ctx;
+ 	 * Canvas context object. Field should not be written to. Used to help for overriding. Inside field `fillstyle` is overwritten with each function.
+	 */
+	ctx;
 	/**
 	 * Canvas constructor. Takes an element ID name as a string. Assumes the correct typing and number of arguments.
  	 */
 	constructor(name) {
-		this.#ctx = document.getElementById(name).getContext("2d");
-		this.#ctx.textBaseline = "top";
+		// Initialise the `ctx` variable.
+		this.ctx = document.getElementById(name).getContext("2d");
+		// Default settings.
+		this.ctx.textBaseline = "top";
+		this.ctx.fontKerning = "normal";
 	}
 	/**
  	 * Rectangle function. Takes a list of arrays in the form [style, x, y, width, height] and returns void. Assumes the correct typing of arguments.
 	 */
 	r(...argv) {
 		for (let avi of argv) {
-			this.#ctx.fillStyle = avi[0];
-			this.#ctx.fillRect(avi[1], avi[2], avi[3], avi[4]);
+			this.ctx.fillStyle = avi[0];
+			this.ctx.fillRect(avi[1], avi[2], avi[3], avi[4]);
 		}
 	}
 	/**
@@ -47,11 +50,11 @@ class Canvas {
 	 */
 	c(...argv) {
 		for (let avi of argv) {
-			this.#ctx.fillStyle = avi[0];
-			this.#ctx.beginPath();
-			this.#ctx.arc(avi[1], avi[2], avi[3], 0, 6.283185307179586);
-			this.#ctx.closePath();
-			this.#ctx.fill();
+			this.ctx.fillStyle = avi[0];
+			this.ctx.beginPath();
+			this.ctx.arc(avi[1], avi[2], avi[3], 0, 6.283185307179586);
+			this.ctx.closePath();
+			this.ctx.fill();
 		}
 	}
 	/**
@@ -59,24 +62,25 @@ class Canvas {
 	 */
 	p(...argv) {
 		for (let avi of argv) {
-			this.#ctx.fillStyle = avi[0];
-			this.#ctx.beginPath();
-			this.#ctx.moveTo(avi[1][0], avi[1][1]);
+			this.ctx.fillStyle = avi[0];
+			this.ctx.beginPath();
+			this.ctx.moveTo(avi[1][0], avi[1][1]);
 			for (let j = 2; j < avi.length; j++) {
-				this.#ctx.lineTo(avi[j][0], avi[j][1]);
+				this.ctx.lineTo(avi[j][0], avi[j][1]);
 			}
-			this.#ctx.closePath();
-			this.#ctx.fill();
+			this.ctx.closePath();
+			this.ctx.fill();
 		}
 	}
 	/**
 	 * Text function. Takes the font then a list of arrays in the form [style, text, x, y] and returns void. Assumes the correct typing of arguments.
 	 */
 	t(font, ...argv) {
-		this.#ctx.font = font;
+		this.ctx.font = font;
 		for (let avi of argv) {
-			this.#ctx.fillStyle = avi[0];
-			this.#ctx.fillText(avi[1], avi[2], avi[3]);
+			this.ctx.fillStyle = avi[0];
+			this.ctx.fontSize = avi[4];
+			this.ctx.fillText(avi[1], avi[2], avi[3]);
 		}
 	}
 }
