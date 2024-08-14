c31a7b9cd11e25b2b91fe1a80c0cad3176c9e469
Coarse Rosinflower <113859223+cD1rtX3@users.noreply.github.com>
2024-08-14 03:55:24 -0400

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,11 +1,22 @@
-import TitleScreen from "./TitleScreen.js";
+import TitleScreen from "TitleScreen.js";
+import ButtonRegistry from "ButtonRegistry.js";
 
 export default class Main {
 	static init(ctx) {
-		//
+		ctx.textBaseline = "middle";
 	}
 	static onKeyDown(e) {}
 	static onKeyUp(e) {}
-	static onClick(e) {}
+	static onClick(e) {
+		for ([button, result] of ButtonRegistry) {
+			if (e.offsetX >= button.x
+				&& e.offsetX <= button.x + button.w
+				&& e.offsetY >= button.y
+				&& e.offsetY <= button.y + button.h) {
+
+				result.call(); break;
+			}
+		}
+	}
 	static refresh() {}
 }
