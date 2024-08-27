7d2990e24e48bf4a23a3dcee0df5ecc8a7cce685
Coarse Rosinflower <113859223+cD1rtX3@users.noreply.github.com>
2024-08-27 10:23:45 -0400

    Update TitleScreen.js

--- TitleScreen.js
+++ TitleScreen.js
@@ -1,21 +1,16 @@
 export default class TitleScreen {
-	ctx;
+	ctx; buttons;
 	constructor(ctx) {
 		this.ctx = ctx;
+		this.buttons = new Array();
 	}
 	draw() {
 		this.drawBackground();
-		this.drawButtons();
+		this.buttons.forEach((button) => button.fill());
 	}
 	drawBackground() {
 		// placeholder background
 		this.ctx.fillStyle = "#0c0c38";
 		this.ctx.fillRect(0, 0, 1280, 720);
-	}
-	drawButtons() {
-		// placeholder buttons
-		this.ctx.fillStyle = "#ffd700";
-		this.ctx.fillRect(590, 350, 100, 20);
-		this.ctx.fillRect(590, 380, 100, 20);
 	}
 }
