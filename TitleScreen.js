66f4946c7776152dcb1d66ac4e3a4b8fdf0a2a53
Coarse Rosinflower <113859223+cD1rtX3@users.noreply.github.com>
2024-10-31 12:14:25 -0400

    Update TitleScreen.js

--- TitleScreen.js
+++ TitleScreen.js
@@ -1,12 +1,23 @@
-import "./Button.js"
+import Button from "./Button.js"
+import ButtonRegistry from "./ButtonRegistry.js";
 
 export default class TitleScreen {
 	ctx; buttons; state;
 	constructor(ctx) {
 		this.ctx = ctx;
 		this.buttons = [
-			new Button(ctx, 490, 360, 300, 150, "#e0dde0", "#010101", "button0", 25),
-			new Button(ctx, 490, 535, 300, 150, "#e0dde0", "#010101", "button1", 25)
+			ButtonRegistry.register(
+				new Button(ctx, 490, 360, 300, 150, "#e0dde0", "#010101", "button0", 25),
+				() => {
+					console.log("penis");
+				}
+			),
+			ButtonRegistry.register(
+				new Button(ctx, 490, 535, 300, 150, "#e0dde0", "#010101", "button1", 25),
+				() => {
+					console.log("penis");
+				}
+			)
 		];
 		this.state = "mainmenu";
 	}
